15cbdd6381bde6a88d5fd5daddc0a1d4348c2fa8
Avi <neuramachine@gmail.com>
2023-04-27 21:51:53 +0300

    refactor to main func

--- main.py
+++ main.py
@@ -2,54 +2,52 @@ import os
 import pandas as pd
 import streamlit as st
 from dotenv import dotenv_values
-import json
 
 from data_importer.api_data_downloader import GoogleNewsAPI
 from data_analysis.chatgpt import StockSentimentAnalyzer
 
-config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 
-# RAPIDAPI_KEY = os.environ.get("RAPIDAPI_KEY")
-# OPENAI_KEY = os.environ.get("OPENAI_KEY")
+def main():
+    config = dotenv_values(".env")
 
-# global_market_api = GlobalStockMarketAPI(config["RAPIDAPI_KEY"])
-# stock_analysis = global_market_api.get_data()
-# stock_analysis_df = pd.DataFrame(stock_analysis)
+    google_news_api = GoogleNewsAPI()
+    google_news_data = google_news_api.get_data("stock market")
+    google_news_df = pd.DataFrame(google_news_data)
 
-google_news_api = GoogleNewsAPI()
-google_news_data = google_news_api.get_data("stock market")
-google_news_df = pd.DataFrame(google_news_data)
+    text = " ".join(google_news_df["desc"])
+    stock_analyzer = StockSentimentAnalyzer(config["OPENAI_KEY"])
+    res = stock_analyzer.analyze_stocks_sentiment(text)
 
-text = " ".join(google_news_df["desc"])
-stock_analyzer = StockSentimentAnalyzer(config["OPENAI_KEY"])
-res = stock_analyzer.analyze_stocks_sentiment(text)
+    # Streamlit app
+    st.title("Stock Market Sentiment Dashboard")
+    st.write("## Google News Data")
+    st.dataframe(google_news_df)
 
-# Streamlit app
-st.title("Stock Market Sentiment Dashboard")
-st.write("## Google News Data")
-st.dataframe(google_news_df)
+    df = pd.DataFrame(res["stocks"])
 
-df = pd.DataFrame(res["stocks"])
+    st.title("Stock Sentiment Analysis")
 
-st.title("Stock Sentiment Analysis")
+    # Create a bar chart for sentiment ratings
+    st.subheader("Sentiment Ratings")
+    chart_data = df[["stock_symbol", "sentiment_rating"]].set_index("stock_symbol")
 
-# Create a bar chart for sentiment ratings
-st.subheader("Sentiment Ratings")
-chart_data = df[["stock_symbol", "sentiment_rating"]].set_index("stock_symbol")
+    # Display reasons in a table
+    st.subheader("Reasons for Sentiment")
 
-# Display reasons in a table
-st.subheader("Reasons for Sentiment")
+    # Create two columns with equal width
+    col1, col2 = st.columns(2)
 
-# Create two columns with equal width
-col1, col2 = st.columns(2)
+    # Display the bar chart in the first column
+    with col1:
+        st.bar_chart(chart_data)
 
-# Display the bar chart in the first column
-with col1:
-    st.bar_chart(chart_data)
+    # Loop through the DataFrame and display the reasons table in the second column
+    with col2:
+        for _, row in df.iterrows():
+            st.write(f"{row['stock_symbol']} Reasons:")
+            reason_data = pd.DataFrame(row["reasons"], columns=["Reasons"])
+            st.table(reason_data)
 
-# Loop through the DataFrame and display the reasons table in the second column
-with col2:
-    for _, row in df.iterrows():
-        st.write(f"{row['stock_symbol']} Reasons:")
-        reason_data = pd.DataFrame(row["reasons"], columns=["Reasons"])
-        st.table(reason_data)
+
+if __name__ == "__main__":
+    main()
